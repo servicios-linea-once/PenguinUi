8fc945731b18f3c6d3da5556edb73c4398970437
Jhon Smith <jsrtantalean@gmail.com>
2025-09-14 00:11:03 -0500

    Update Dropdown.php

--- src/View/Components/Dropdown.php
+++ src/View/Components/Dropdown.php
@@ -51,7 +51,7 @@ class Dropdown extends Component
                 @endif
                
                 <!-- Dropdown Menu -->
-                <div x-cloak wire:key="dropdown-slot-{{ $uuid }}" x-show="isOpen || openedWithKeyboard" x-transition x-trap="openedWithKeyboard" x-on:click.outside="isOpen = false, openedWithKeyboard = false" x-on:keydown.down.prevent="$focus.wrap().next()" x-on:keydown.up.prevent="$focus.wrap().previous()" @class(["absolute flex w-fit min-w-48 flex-col overflow-hidden rounded-radius border border-outline bg-surface-alt dark:border-outline-dark dark:bg-surface-dark-alt z-auto", $positionClass]) role="menu">
+                <div x-cloak wire:key="dropdown-slot-{{ $uuid }}" x-show="isOpen || openedWithKeyboard" x-transition x-trap="openedWithKeyboard" x-on:click.outside="isOpen = false, openedWithKeyboard = false" x-on:keydown.down.prevent="$focus.wrap().next()" x-on:keydown.up.prevent="$focus.wrap().previous()" @class(["absolute flex w-fit min-w-48 flex-col overflow-hidden rounded-radius border border-outline bg-surface-alt dark:border-outline-dark dark:bg-surface-dark-alt z-20", $positionClass]) role="menu">
                     {{ $slot }}
                 </div>
             </div>
